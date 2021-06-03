27f4a80120644a76895ab8f0dc5ed9132a5ed582
gabybaty <batistuta9.gb@gmail.com>
2021-06-03 02:12:22 -0300

    listo

--- app.js
+++ app.js
@@ -11,5 +11,5 @@ app.get('/hamilton',(req,res)=> res.sendFile(path.join(views,'hamilton.html')));
 app.get('/hopper',(req,res)=> res.sendFile(path.join(views,'hopper.html')));
 app.get('/lovelace',(req,res)=> res.sendFile(path.join(views,'lovelace.html')));
 app.get('/turing',(req,res)=> res.sendFile(path.join(views,'turing.html')));
-app.listen(path,()=> console.log ('Servidor corriendo en el puerto ' + port));
+app.listen(port,()=> console.log ('Servidor corriendo en el puerto ' + port));
 app.use(express.static('public'));
