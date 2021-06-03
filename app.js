d3ecc09ea7f4485f8fa1026709e0755bef4ed4b1
gabybaty <batistuta9.gb@gmail.com>
2021-06-03 01:48:08 -0300

    listo el app

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3030;
+const path = require('path');
+const views = path.join(__dirname,'views');
+app.get('/',(req,res)=> res.sendFile(path.join(views,'index.html')));
+app.get('/babbage',(req,res)=> res.sendFile(path.join(views,'babbage.html')));
+app.get('/berners',(req,res)=> res.sendFile(path.join(views,'berners.html')));
+app.get('/clarke',(req,res)=> res.sendFile(path.join(views,'clarke.html')));
+app.get('/hamilton',(req,res)=> res.sendFile(path.join(views,'hamilton.html')));
+app.get('/hopper',(req,res)=> res.sendFile(path.join(views,'hopper.html')));
+app.get('/lovelace',(req,res)=> res.sendFile(path.join(views,'lovelace.html')));
+app.get('/turing',(req,res)=> res.sendFile(path.join(views,'turing.html')));
+app.listen(path,()=> console.log ('Servidor corriendo en el puerto ' + port));
+app.use(express.static('public'));
